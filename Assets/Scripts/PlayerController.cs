53bba2b2ed64cff4a64b34bbdf4f4d096f1b6296
Andrew Huntington <andrew.huntington@gmail.com>
2024-01-02 21:31:51 +0900

    Add basic movement for player ship

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  [SerializeField] float speed = 10f;
+  private float leftBoundary = -18.77f;
+  private float rightBoundary = 17f;
+  private float upperBoundary = 12.35f;
+  // may need to adjust once ground is added
+  private float lowerBoundary = -5f;
 
-    // Update is called once per frame
-    void Update()
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    float horizontalInput = Input.GetAxis("Horizontal");
+    float verticalInput = Input.GetAxis("Vertical");
+
+    transform.Translate(horizontalInput * speed * Time.deltaTime * Vector3.right);
+    transform.Translate(verticalInput * speed * Time.deltaTime * Vector3.up);
+  }
+
+  private void OnTriggerEnter(Collider other)
+  {
+    if (other.CompareTag("Enemy"))
     {
-        
+      Debug.Log("CRASH!");
     }
+  }
 }
